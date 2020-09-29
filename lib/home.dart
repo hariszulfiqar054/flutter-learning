6ffbdf5f4b39a95d3e427baffb2f6fd18a6e04ef
Muhammad Haris <haris.skylinxtech@gmail.com>
2020-09-29 16:01:46 +0500

    inintial commit

--- lib/home.dart
+++ lib/home.dart
@@ -8,10 +8,11 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<Qoutes> name = [
-    Qoutes(name: 'Haris', age: 12),
+    Qoutes(name: 'Haris is a good guy', age: 12),
     Qoutes(name: 'zaid', age: 13),
     Qoutes(name: 'ali', age: 11)
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,8 +24,54 @@ class _HomeState extends State<Home> {
         centerTitle: true,
       ),
       body: Column(
-        children:
-            name.map((e) => Text('${e.name}' + ' ' + '${e.age}')).toList(),
+        children: name
+            .map((e) => new QouteCard(
+                qoute: e,
+                onDelete: () {
+                  setState(() {
+                    name.remove(e);
+                  });
+                }))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class QouteCard extends StatelessWidget {
+  final Qoutes qoute;
+  final Function onDelete;
+  QouteCard({Key key, this.qoute, this.onDelete}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(16, 16, 16, 2),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              qoute.name,
+              style: TextStyle(fontSize: 18, color: Colors.grey[600]),
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            Text(
+              qoute.age.toString(),
+              style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            FlatButton.icon(
+                onPressed: onDelete,
+                icon: Icon(Icons.delete),
+                label: Text('Press to delete'))
+          ],
+        ),
       ),
     );
   }
